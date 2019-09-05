f3d9e89178e170112412b2d7016ff7810df75019
KumaTea <36222458+KumaTea@users.noreply.github.com>
2019-09-06 03:32:12 +0800

    0.1.0.1

--- mdSYSU.py
+++ mdSYSU.py
@@ -7,6 +7,11 @@ import random
 SYSU = ['中山大学', '中大', '中带', '双鸭山', '鸭大', '鸭鸭山', '双倍多多鸭', ]
 student = ['学生', '学子', '大佬', '带佬', '高雅人士', '大手子', ]
 greet_msg = ['快出来高强度水群', '学习使恁快乐', '憋睡辣', '高雅人士早就醒力', '起来批判一番']
+greet_sticker = ['CAADBQADZQMAAtrNfQNfdW_Fq9VLjBYE', 'CAADBQADVgEAArL6ew4JTyVTbRYQlBYE',
+                 'CAADBQADRQEAArL6ew6aNvPHLuAFDRYE', 'CAADBQADHAIAArL6ew7w7ukRYBfgERYE',
+                 'CAADAQADOwEAAjg9ixwrtVwD8eJ00hYE', 'CAADAQAD_wADOD2LHOJSQakxlemCFgQ',
+                 'CAADBQADRAEAArL6ew7rEnfWOPQ_dhYE', 'CAADBQADEgADpc1iJrCMCke01ilSFgQ',
+                 ]
 
 
 def check_connection(url):
@@ -37,8 +42,9 @@ def send_flood():
 def send_greetings():
     temp, temp_max, weather_desc = mdWeather.check_forecast()
     grt_msg = f'各位{random.choice(SYSU)}的{random.choice(student)}早上好！\n' \
-              f'今日{weather_desc}，{temp}~{temp_max}度。\n' \
+              f'今日{weather_desc}，{temp}~{temp_max}度。\n\n' \
               f'{random.choice(greet_msg)}，，，'
 
     kuma.send(token_db.chat['sbddy']).text(grt_msg)
+    kuma.send(token_db.chat['sbddy']).sticker(random.choice(greet_sticker))
     return True
