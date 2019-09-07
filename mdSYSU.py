3f36e356f5124c80b6773e126d9bf27af45271ca
KumaTea <36222458+KumaTea@users.noreply.github.com>
2019-09-08 02:05:50 +0800

    0.1.1.1 incomplete

--- mdSYSU.py
+++ mdSYSU.py
@@ -13,6 +13,20 @@ greet_sticker = ['CAADBQADZQMAAtrNfQNfdW_Fq9VLjBYE', 'CAADBQADVgEAArL6ew4JTyVTbR
                  'CAADBQADRAEAArL6ew7rEnfWOPQ_dhYE', 'CAADBQADEgADpc1iJrCMCke01ilSFgQ',
                  ]
 
+Guangzhou_code = 101280101
+Zhuhai_code = 101280701
+Shenzhen_code = 101280601
+
+dow_cn = {
+    0: '一',
+    1: '二',
+    2: '三',
+    3: '四',
+    4: '五',
+    5: '六',
+    6: '天',
+}
+
 
 def check_connection(url):
     try:
@@ -40,13 +54,18 @@ def send_flood():
 
 
 def send_greetings():
-    g_temp, g_temp_max, g_weather_desc = mdWeather.check_forecast()
-    z_temp, z_temp_max, z_weather_desc = mdWeather.check_forecast(2052479)
-    grt_msg = f'各位{random.choice(SYSU)}的{random.choice(student)}早上好！\n' \
-              f'今日广州{g_weather_desc}，{g_temp}~{g_temp_max}度；\n' \
-              f'今日珠海{z_weather_desc}，{z_temp}~{z_temp_max}度。\n\n' \
+    g_temp, g_temp_max, g_weather_desc = mdWeather.check_forecast(Guangzhou_code)
+    z_temp, z_temp_max, z_weather_desc = mdWeather.check_forecast(Zhuhai_code)
+    s_temp, s_temp_max, s_weather_desc = mdWeather.check_forecast(Shenzhen_code)
+    month, day, weekday = mdWeather.check_date()
+    lunar_month, lunar_day, lunar_term = mdWeather.check_lunar()
+    grt_msg = f'各位{random.choice(SYSU)}的{random.choice(student)}早上好！' \
+              f'今天是{month}月{day}日{dow_cn[weekday]}，农历{lunar_month}月{lunar_day}{lunar_term}。\n' \
+              f'广州{g_weather_desc}，{g_temp}~{g_temp_max}度；' \
+              f'珠海{z_weather_desc}，{z_temp}~{z_temp_max}度；' \
+              f'深圳{s_weather_desc}，{s_temp}~{s_temp_max}度。\n\n' \
               f'{random.choice(greet_msg)}，，，'
 
-    kuma.send(localDB.chat['sbddy']).text(grt_msg)
-    kuma.send(localDB.chat['sbddy']).sticker(random.choice(greet_sticker))
+    kuma.send(localDB.chat['me']).text(grt_msg)
+    kuma.send(localDB.chat['me']).sticker(random.choice(greet_sticker))
     return True
