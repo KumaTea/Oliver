2f04939b85099bd375f61c2aea9f68ef1f5c252f
KumaTea <36222458+KumaTea@users.noreply.github.com>
2019-09-14 03:24:18 +0800

    0.2.2.1 bugfix

--- taskManager.py
+++ taskManager.py
@@ -13,5 +13,5 @@ def manager():
     scheduler.add_job(task_check, 'cron', hour='*/4')
     scheduler.add_job(sync_posts, 'cron', hour=6)
     scheduler.add_job(send_post, 'cron', hour='0,4,8,12,16,20')
-    scheduler.add_job(send_con, 'cron', hour=7, minute=59)
+    scheduler.add_job(send_con, 'cron', hour=8, minute=30)
     scheduler.add_job(do_backup, 'cron', day_of_week='sat', hour=2)
