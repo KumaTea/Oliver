64dd0639e655e7ba0ca373b4e1a7d9f565a2d799
KumaTea <36222458+oudoubleyang@users.noreply.github.com>
2019-09-14 03:29:33 +0800

    0.2.2.1 bugfix 2

--- mdBackup.py
+++ mdBackup.py
@@ -6,11 +6,11 @@ from botSession import kuma, task_done
 
 
 def do_backup():
-    p = subprocess.Popen(['powershell.exe', 'backup.ps1'],
+    p = subprocess.Popen(['powershell.exe', 'C:/Users/Administrator/Documents/Oliver/backup.ps1'],
                          stdout=sys.stdout)
     p.communicate()
     year, month, date = datetime.now().strftime('%Y'), datetime.now().strftime('%m'), datetime.now().strftime('%d')
-    filepath = f'D:\\FS\\Backup\\{year}{month}{date}.zip'
+    filepath = f'D:/FS/Backup/{year}{month}{date}.zip'
     kuma.send(localDB.chat['me']).file(filepath, upload=True)
     task_done('backup')
     return True
