01d2f35dc3bcce6f05fc41191b56a4993d791b83
KumaTea <36222458+oudoubleyang@users.noreply.github.com>
2019-09-22 04:08:15 +0800

    0.3.0.1 Avoid import recursion

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 import os
-from tools import starting_tasks
+from taskManager import *
 from tgapi.tools import set_proxy
 
 
@@ -19,6 +19,36 @@ def mkdir(folder=None):
                     os.mkdir(str(items))
         else:
             os.mkdir(str(folder))
+
+
+def starting_tasks(filename='start_tasks.txt'):
+    i = 0
+    with open(filename, 'r') as f:
+        tasks = f.read().split('\n')
+    if 'send_greetings' in tasks:
+        send_greetings()
+        i += 1
+    if 'send_flood' in tasks:
+        send_flood()
+        i += 1
+    if 'send_con' in tasks:
+        send_con()
+        i += 1
+    if 'task_check' in tasks:
+        task_check()
+        i += 1
+    if 'sync_posts' in tasks:
+        sync_posts()
+        i += 1
+    if 'send_post' in tasks:
+        send_post()
+        i += 1
+    if 'do_backup' in tasks:
+        do_backup()
+        i += 1
+    with open(filename, 'w') as f:
+        f.write('')
+    return True if i > 0 else None
 
 
 def starting():
