ba39c47df13f9749455e4cf8d2d1ca328a897d4e
KumaTea <36222458+KumaTea@users.noreply.github.com>
2020-07-10 13:29:45 +0800

    update backup

--- mdHostCmd.py
+++ mdHostCmd.py
@@ -1,4 +1,5 @@
 import localDB
+import subprocess
 from botSession import kuma
 from botTools import task_done
 from datetime import datetime
@@ -6,10 +7,10 @@ from datetime import datetime
 
 def do_backup():
     year, month, date = datetime.now().strftime('%Y'), datetime.now().strftime('%m'), datetime.now().strftime('%d')
-    with open('../vote/vote.json', 'rb') as f:
-        kuma.send_document(localDB.chat['me'], f)
-    with open('tum/posts.p', 'rb') as f:
+    subprocess.run(['zip', '-r', '-9', f'{localDB.home_dir}/backup/{year}{month}{date}.zip',
+                    f'{localDB.home_dir}/Oliver/tum', f'{localDB.home_dir}/Dragalia/life',
+                    f'{localDB.home_dir}/vote'])
+    with open(f'{localDB.home_dir}/backup/{year}{month}{date}.zip', 'rb') as f:
         kuma.send_document(localDB.chat['me'], f)
     kuma.send_message(localDB.chat['me'], f'Backup files on {year}-{month}-{date}')
-    task_done('backup')
-    return True
+    return task_done('backup')
